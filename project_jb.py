6bb995f2d04b88a4ae12ee33402c8d5296465a3d
jbmag <jean-baptiste.magnin@epfl.ch>
2020-05-01 23:52:58 +0200

    objects extracted

--- project_jb.py
+++ project_jb.py
@@ -7,74 +7,12 @@ from skimage import morphology
 #extract frames from video
 im = myfu.extract_frames("robot_parcours_1.avi")
 
-#average over every frame and over the 3 channels to remove robot
-test = im.astype('long').mean(axis=0)
-test = test.mean(axis=2).astype('uint8')
+#extracts digits and operands from frames  
+all_objects = myfu.get_operands(im, avoid_shaky_plus=True)
 
-#gaussian adaptive threshold
-test=cv2.adaptiveThreshold(test,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,151,20)
-
-#morphology: remove small holes
-kernel = np.ones((3,3),np.uint8)
-test = morphology.remove_small_holes(test, 20)
-# f2=plt.figure(4)
-# plt.imshow(test, cmap='gray')
-
-
-# #do the same without averaging over all the frames
-# test2 = im[0,:,:,:].copy()
-# test2 = test2.mean(axis=2).astype('uint8')
-
-# #gaussian adaptive threshold
-# test2=cv2.adaptiveThreshold(test2,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-#             cv2.THRESH_BINARY,151,20)
-
-# #morphology: remove small holes
-# kernel = np.ones((3,3),np.uint8)
-# test2 = morphology.remove_small_holes(test2, 20)
-# f2=plt.figure(5)
-# plt.imshow(test2, cmap='gray')
-# plt.show()
-
-# #remove both shaky plus sign and robot
-# print(test.shape, test2.shape)
-# test[test!=test2]=True
-# plt.imshow(test, cmap='gray')
-# plt.show()
-
-#re-convert to uint8 with  value from 0 to 255
-test = test.astype('uint8')
-test[test == 1] = 255
-
-
-#count objects with code from lab1
-label = myfu.count_objects(test).astype('uint8')
-
-
-
-#check num and size of object, remove big objects and their label
-for i in range(label.max()):
-    size = (label==(i+1)).sum()
-    while size >=700:
-        label[label==(i+1)] = 0
-        for j in range(i+1, label.max()):
-            label[label==(j+1)] = j
-        size = (label==(i+1)).sum()
-
-
-#extract mini-image of each object, as well as their center
-obj1 = label==1
-index = np.nonzero(obj1)
-left = index[1].min() - 10
-right = index[1].max() + 10
-top = index[0].min() - 10
-bottom = index[0].max() + 10
-print(top, bottom, left, right)
-
-f=plt.figure(1)
-plt.imshow(label==1)
-
-f=plt.figure(2)
-plt.imshow(label[top:bottom, left:right])
+# Plot images
+fig, axes = plt.subplots(1, all_objects.shape[0])
+for ax, im in zip(axes, all_objects):
+    ax.imshow(im, cmap='gray')
+    ax.axis('off')
 plt.show()
--- some_functions.py
+++ some_functions.py
@@ -1,20 +1,9 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage import morphology
 
-
-#function to change illumination of the image, using the mean computed before
-def normalize_image(im, means_ch):
-    r_im = im.astype('int16')
-    for nb_ch in range(3):
-        r_im[:,:,nb_ch] = r_im[:,:,nb_ch] - (np.mean(r_im[:,:,nb_ch]) - means_ch[nb_ch])
-        r_im[r_im[:,:,nb_ch] >= 256, nb_ch] = 255
-        r_im[r_im[:,:,nb_ch] < 0, nb_ch] = 0
-    r_im = r_im.astype('uint8')
-    return r_im
-
-
-
+#extract every frame from a video
 def extract_frames(video_file_name):
     vid_cap = cv2.VideoCapture(video_file_name)
     if vid_cap.isOpened() is False:
@@ -33,42 +22,15 @@ def extract_frames(video_file_name):
     return np.flip(frames.astype('uint8'), axis=-1) #flip because R and B channels are inversed 
 
 
-def white_black_blue_red(im):
-    white=np.array([255, 255, 255], dtype='float')
-    black=np.array([0, 0, 0], dtype='float')
-    blue=np.array([0, 0, 255], dtype='float')
-    red=np.array([255, 0, 0], dtype='float')
-    distance_to_color = np.zeros([im.shape[0], im.shape[1], 4], dtype='float')
-    distance_to_color[:,:,0] = np.sqrt(((im.astype('float') - white)**2).sum(axis=2))
-    distance_to_color[:,:,1] = np.sqrt(((im.astype('float') - black)**2).sum(axis=2))
-    distance_to_color[:,:,2] = np.sqrt(((im.astype('float') - blue)**2).sum(axis=2))
-    distance_to_color[:,:,3] = np.sqrt(((im.astype('float') - red)**2).sum(axis=2))
-    ret = distance_to_color.argmin(axis=2)
-    ret_im = np.zeros(im.shape)
-    ret_im[ret==0,:]=255
-    ret_im[ret==1,:]=0
-    ret_im[ret==2,2]=255
-    ret_im[ret==3,0]=255
-    return ret_im.astype('uint8')
-
-def find_arrow(image):
-    #the arrow is the only red part
-    arrow = image[:,:,0]
-    arrow[np.logical_and(image[:,:,0] > 100, image[:,:,2] < 50)] = 255
-    arrow[np.logical_or(image[:,:,0] <= 100, image[:,:,2] >= 50)] = 0
-    plt.imshow(arrow)
-    plt.show()
 
 
-#separate each shape
-
+#from Lab1:
 #functions that returns a bool array with True values at the pixels a radius of size-pixels around pixel with coordinate (line, col)
 def get_neighbours_coord(line, col, size, shape):
     neighb = np.zeros(shape, dtype=bool)
     neighb[(line-size):(line+size+1), (col-size):(col+size+1)] = True
     return neighb
-    
-    
+      
 #function to count object in a grey_scale image, with removed background
 def count_objects(image):
     shape_num = 1
@@ -86,3 +48,94 @@ def count_objects(image):
                     label[np.logical_and(image != 255, neighb)] = label[neighb].max()
     return label
                     
+
+
+def get_operands(images, avoid_shaky_plus=False):
+    #average over every frame and over the 3 channels to remove robot
+    m_im = images.astype('long').mean(axis=0)
+    m_im = m_im.mean(axis=2).astype('uint8')
+
+    #gaussian adaptive threshold
+    m_im=cv2.adaptiveThreshold(m_im,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                cv2.THRESH_BINARY,151,20)
+
+    #morphology: remove small holes
+    m_im = morphology.remove_small_holes(m_im, 10)
+    
+    if avoid_shaky_plus:
+        #do the same without averaging over all the frames, using only first frame
+        m_im2 = images[0,:,:,:].copy()
+        m_im2 = m_im2.mean(axis=2).astype('uint8')
+
+        #gaussian adaptive threshold
+        m_im2=cv2.adaptiveThreshold(m_im2,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                    cv2.THRESH_BINARY,151,20)
+
+        #morphology: remove small holes
+        m_im2 = morphology.remove_small_holes(m_im2, 10)
+        
+        #use both to remove both shaky plus sign and robot
+        m_im[m_im!=m_im2]=True
+
+    #re-convert to uint8 with  value from 0 to 255
+    m_im = m_im.astype('uint8')
+    m_im[m_im == 1] = 255
+
+    #count objects with code from lab1
+    label = count_objects(m_im).astype('uint8')
+    
+    #check num and size of object, remove big objects and their label
+    for i in range(label.max()):
+        size = (label==(i+1)).sum()
+        while size >=700 or (size <40 and size >0):
+            label[label==(i+1)] = 0
+            for j in range(i+1, label.max()):
+                label[label==(j+1)] = j
+            size = (label==(i+1)).sum()
+    
+    #extract mini-image of each object, as well as their center
+    list_objects = []
+    for i in range(label.max()):
+        obj = label==(i+1) #select only object with label i+1
+        index = np.nonzero(obj) #find the index of every pixel of the object
+        left = index[1].min() - 10 #get the bounds of the index
+        right = index[1].max() + 10
+        top = index[0].min() - 10
+        bottom = index[0].max() + 10
+        list_objects.append(label[top:bottom, left:right])
+    
+    #padd the mini-image so that they have the same shape
+    heights = np.zeros(len(list_objects), dtype='int')
+    widths = np.zeros(len(list_objects), dtype='int')
+    for i in range(len(list_objects)):
+        heights[i] = list_objects[i].shape[0]
+        widths[i] = list_objects[i].shape[1]
+    height = heights.max()
+    width = widths.max()
+    all_objects = np.zeros((len(list_objects), height, width))
+    for i in range(len(list_objects)):
+        vert = height - list_objects[i].shape[0]
+        horiz = width - list_objects[i].shape[1]
+        print()
+        if vert > 0 and vert%2==0:
+            if horiz > 0 and horiz%2==0:
+                all_objects[i,:,:] = np.pad(list_objects[i], ((int(vert/2),int(vert/2)),(int(horiz/2),int(horiz/2))), mode = 'constant')
+            elif horiz > 0:
+                all_objects[i,:,:] = np.pad(list_objects[i], ((int(vert/2),int(vert/2)),(int((horiz-1)/2),int((horiz+1)/2))), mode = 'constant')
+            elif horiz == 0:
+                all_objects[i,:,:] = np.pad(list_objects[i], ((int(vert/2),int(vert/2)),(0,0)), mode = 'constant')
+        elif vert > 0:
+            if horiz > 0 and horiz%2==0:
+                all_objects[i,:,:] = np.pad(list_objects[i], ((int((vert-1)/2),int((vert+1)/2)),(int(horiz/2),int(horiz/2))), mode = 'constant')
+            elif horiz > 0:
+                all_objects[i,:,:] = np.pad(list_objects[i], ((int((vert-1)/2),int((vert+1)/2)),(int((horiz-1)/2),int((horiz+1)/2))), mode = 'constant')
+            elif horiz == 0:
+                all_objects[i,:,:] = np.pad(list_objects[i], ((int((vert-1)/2),int((vert+1)/2)),(0,0)), mode = 'constant')
+        elif vert == 0:
+            if horiz > 0 and horiz%2==0:
+                all_objects[i,:,:] = np.pad(list_objects[i], ((0,0),(int(horiz/2),int(horiz/2))), mode = 'constant')
+            elif horiz > 0:
+                all_objects[i,:,:] = np.pad(list_objects[i], ((0,0),(int((horiz-1)/2),int((horiz+1)/2))), mode = 'constant')
+            elif horiz == 0:
+                all_objects[i,:,:] = np.pad(list_objects[i], ((0,0),(0,0)), mode = 'constant')
+    return all_objects
